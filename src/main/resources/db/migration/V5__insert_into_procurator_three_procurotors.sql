c32ef07f39d54d9624a715981c3e68ee57409dc4
曹桂凤 <demi.cao@oocl.com>
2019-07-18 18:03:00 +0800

    [Demi]feat:implement:story4 ac1 insert three procutors into procurator

---
+++ src/main/resources/db/migration/V5__insert_into_procurator_three_procurotors.sql
@@ -0,0 +1,3 @@
+INSERT INTO procurator (id,justice_name,sex,age,birthday,id_number,experience)VALUES (1,'张三','男',19,'2001-10-20','001',3);
+INSERT INTO procurator (id,justice_name,sex,age,birthday,id_number,experience)VALUES (2,'李四','女',28,'2001-10-20','002',2);
+INSERT INTO procurator (id,justice_name,sex,age,birthday,id_number,experience)VALUES (3,'王五','男',58,'2001-10-20','003',5);
